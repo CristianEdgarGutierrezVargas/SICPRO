88018a0472e75488666166c8c6a43eeb13a88673
marco_chipana <chipanamarco001@gmail.com>
2024-02-04 11:41:22 -0400

    Componentes-common

---
+++ Logica/Consumo/ConsumoCommon.cs
@@ -0,0 +1,49 @@
+﻿using Common;
+using EntidadesClases.ModelSicPro;
+using ManejadorMetodos.CDBSicPro;
+using ManejadorModelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.Consumo
+{
+    public class ConsumoCommon
+    {
+        #region Contructor Principal
+
+        private readonly Cgr_componente _manejador_gr_componente;
+
+        public static sicproEntities dbContext;
+        public ConsumoCommon()
+        {
+            //if (dbContext != null) dbContext.Dispose();
+            dbContext = new sicproEntities();
+            _manejador_gr_componente = new Cgr_componente(dbContext);
+        }
+
+        #endregion
+
+        #region gr_componente
+
+        public List<vgr_comprol> ObtenerTablaComp(decimal idComponente, long idRol)
+        {
+            try
+            {
+                return _manejador_gr_componente.ObtenerTablaComp(idComponente, idRol);
+            }
+            catch (SecureExceptions secureException)
+            {
+                throw new SecureExceptions("Error al Generar la Transacción", secureException);
+            }
+            finally
+            {
+                //dbContext.Dispose();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ ManejadorMetodos/CDBSicPro/Cgr_componente.cs
@@ -0,0 +1,40 @@
+﻿using Common;
+using EntidadesClases.ModelSicPro;
+using ManejadorModelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManejadorMetodos.CDBSicPro
+{
+    public class Cgr_componente
+    {
+        #region Contructor Principal
+
+        readonly sicproEntities _context;
+        public Cgr_componente(sicproEntities dbContext)
+        {
+            _context = dbContext;
+        }
+
+        #endregion
+
+        public List<vgr_comprol> ObtenerTablaComp(decimal idComponente, long idRol)
+        {
+            try
+            {
+                var sql = _context.vgr_comprol.Where(w => w.dep_com == idComponente && w.id_rol == idRol).OrderBy(o => o.id_com).ToList();
+                return sql;
+                //string sentenciaSQL = "SELECT * FROM vgr_comprol WHERE vgr_comprol.dep_com = " + var1 + " " +
+                //    "AND vgr_comprol.id_rol = " + var2 + " ORDER BY id_com ASC";
+               
+            }
+            catch (SecureExceptions original)
+            {
+                throw new SecureExceptions("Error al Generar la Consulta", original);
+            }
+        }
+    }
+}
